b80a38ceb7df6413ba9b0fe1fae2f140b8aede7d
BlockJJam <blockjjam99@gmail.com>
2021-08-22 22:33:45 +0900

    feat: check box handling in Spring
    식pring에서는 'on'을 'true'로 인식한다, 체크박스를 선택안하면 아예 open값이 넘어오지도 않는다
    체크박스에 해당하는 객체(= open)가 null로 넘어올 때 문제가 되는 경우, 서버가 해당 값을 인식안하고 넘어가버리는 경우도 있다
    문제 해결 방법: 히든 태그를 이용 -> 체크가 안되어있으면, _open = on 으로 전송, 그러면 open = false로 spring은 인식
    타임 리프에서는 다른 방식으로 해결가크

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -39,6 +41,10 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen());       // spring에서는 'on'을 'true'로 인식한다, 체크박스를 선택 안하면 아예 open값이 넘어오지도 않는다
+                                                        // 체크박스에 해당하는 객체(= open)가 null로 넘어올 때 문제가 되는 경우, 서버가 해당 값을 인식안하고 넘어가버리는 경우도 있다
+                                                        // 문제 해결 방법: 히든 태그를 이용 -> 체크가 안되어있으면, _open = on 으로 전송, 그러면 open = false로 spring은 인식
+                                                        // 타임 리프에서는 다른 방식으로 해결가크
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
