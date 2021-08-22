b889d03f00ed41778c780f80c8939f3e292826fa
BlockJJam <blockjjam99@gmail.com>
2021-08-22 23:29:15 +0900

    feat: multi check box using thymeleaf
     멑티체크박스를 타임리프에서 사용하는 방법을 사용해본다
     추가로 @ModelAttribute 메서드로 공통 모델처리 하는 것
     th:for="${#ids.prev('region')}"을 학습함

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,21 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /**
+     * 중복으로 Model에 넣어줘야 하는 재료들을 한번에 중복없이 담아줘보자
+     * 자동으로 모델에 담김
+     * @return
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions(){
+        Map<String,String> regions= new LinkedHashMap<>(); // 순서 보장을 위해(약간 느림)
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -36,6 +53,7 @@ public class FormItemController {
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item",new Item());
+
         return "form/addForm";
     }
 
@@ -45,6 +63,8 @@ public class FormItemController {
                                                         // 체크박스에 해당하는 객체(= open)가 null로 넘어올 때 문제가 되는 경우, 서버가 해당 값을 인식안하고 넘어가버리는 경우도 있다
                                                         // 문제 해결 방법: 히든 태그를 이용 -> 체크가 안되어있으면, _open = on 으로 전송, 그러면 open = false로 spring은 인식
                                                         // 타임 리프에서는 다른 방식으로 해결가능
+        log.info("item.regions={}", item.getRegions());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
